3c89d566bdd20e7c544610504b6bb1ee336223fd
christopherchinia <chinia214@gmail.com>
2022-05-15 16:23:17 -0400

    deleting codes in the boxquiz section

--- Assets/Javascript/main.js
+++ Assets/Javascript/main.js
@@ -51,13 +51,7 @@ const quiz = [
     }
 ]
 
-// Getting all required elements
-const proceedbtn = document.querySelector(".proceed");
-const quizbox = document.querySelector(".quizbox");
-
-// if proceed clicked
-proceedbtn.onclick = ()=>{
-    boxmodel.classList.remove("activeBoxmodel");  // hide the boxmodel 
-    quizbox.classList.add("activeQuiz");  // show the quizbox
-}
+let count = 0;
+let score = 0;
+co
 
